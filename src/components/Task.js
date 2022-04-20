096dd233eafd2649c06c4466349557ded505ef3d
Brayan Castillo <bhcastillor@gmail.com>
2022-04-20 17:27:08 -0500

    change task background to red

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,7 +8,7 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           defaultChecked={state === 'TASK_ARCHIVED'}
           disabled={true}
           name="checked"
-          style={{ background: 'blue' }}
+          style={{ background: 'red' }}
         />
         <span
           className="checkbox-custom"
